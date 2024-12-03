2b8a0de234e70388a1a05775a38ec069dfa8791e
MELAA <melaa@MacBook-Pro-de-MELAA.local>
2024-12-04 00:38:21 +0100

    creation de search

--- frontend/filmeo/src/components/search/Search.jsx
+++ frontend/filmeo/src/components/search/Search.jsx
@@ -2,37 +2,56 @@ import React from 'react';
 import { useState, useEffect } from 'react';
 
 
-export default function Search(){
+export default function Search() {
+    <script crossorigin src="https://api.tvmaze.com"></script>
 
     const [search, setSearch] = useState('');
     const [resultSearch, setResultSearch] = useState([]);
 
-    
 
-    useEffect(()=>{
-        fetch(`https://api.tvmaze.com/singlesearch/shows?q=${search}`)
-    .then(response => response.json())
-    .then(data => setResultSearch(data))}
-    ,[search])
 
-    const onSearch = (e) =>{
+    useEffect(() => {
+        const PROXY = 'https://api.allorigins.win/raw?url='; //autorisation pour le cross origine
+        fetch(`${PROXY}https://api.tvmaze.com/search/shows?q=${search}`)
+        .then(response => response.json())
+        .then(data => setResultSearch(data))
+        .catch(error => console.error(error));
+
+
+
+
+
+        /*const PROXY = 'https://api.allorigins.win/raw?url-';
+        fetch('https://api.tvmaze.com/recherche/shows?q-girls')
+        //fetch(`https://api.tvmaze.com/recherche/shows?q-${search}`)
+            .then(response => response.json())
+            .then(data => setResultSearch(data))*/
+    }
+        , [search])
+
+    const onSearch = (e) => {
         //console.log(e.target.value);
         setSearch(e.target.value);
-        
     }
 
 
-  return (
+    return (
         <>
             <h1>search</h1>
             <label htmlFor="recherche">recherche:</label>
             <input type="text" onChange={onSearch} />
 
+
             <div>
-                {resultSearch.map(()=)}
+
+                {resultSearch && resultSearch.map((result) => (console.log(result.show)
+                
+                ))
+                }
+
             </div>
         </>
-  )
+    )
 
 
 }
