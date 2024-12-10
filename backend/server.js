58b759387bed8e6d5322f83cf23ffe713515393b
melaa <melaa976@gmail.com>
2024-12-10 10:07:44 +0100

    Mse en place du backend

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
+//point d'entrée de l'application 
 const express = require('express');
 const mongoose = require('mongoose');
+require('dotenv').config //charger les variable d'nvironnement
+const connectDB = require('./config/db') //connection à mangoDB
 
 
-
-
+// Création de l'instance express
 const app = express();
-const port = process.env.PORT || 3000 //gestion des ports
 
-//Configuration des middlewares
-app.use(express.json());
-app.use
+
+//gestion des ports, utilisation du port dans le variable d'environnement
+//ou le 3000 par defaut
+const port = 3000 
+
 
 //connection à la basse de donnéess
-
-mongoose.connect()
+mongoose.connect('mongodb://localhost:27017/filmeo');
 
 app.get('/', (req, res) => {
     res.send('Bienvenue sur ma page')
 })
+
+app.listen(port, () => {
+    console.log(`app listening on port ${port}`)
+  })
+  
