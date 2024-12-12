064c158acc4e4933a242e658817f704db6c93721
melaa <melaa976@gmail.com>
2024-12-12 16:29:05 +0100

    12/12/2024

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,8 @@
 const mongoose = require("mongoose");
 const {Schema} = mongoose;
 require('mongoose-type-email');
+var mongoose = require('mongoose');
+var uniqueValidator = require('mongoose-unique-validator');
 
 mongoose.SchemaTypes.Email.defaults.message = 'Address mail invalide'
 
@@ -9,28 +11,52 @@ const userSchema = new Schema(
     {
         nom: {
             type: String,
-            require: [true, 'Le nom est obligatoire'],
-            trim: true
+            required: [true, 'Le nom est obligatoire'],
+            trim: true, //supprime les espaces unitiles
+            minlength : [2, 'Le nom doit contenir au moins 2 caractères '],
+            minlength : [50, 'Le nom ne peut pas dépasser 50 caractères '],
+
         },
 
         prenom: {
             type: String,
-            require: [true, 'Le nom est obligatoire'],
-            trim: true
+            required: [true, 'Le nom est obligatoire'],
+            trim: true, //supprime les espaces unitiles
+            minlength : [2, 'Le prenom doit contenir au moins 2 caractères '],
+            minlength : [50, 'Le prenom ne peut pas dépasser 50 caractères '],
+
         },
 
         mail: {
-            email: mongoose.SchemaTypes.Email
+            type: mongoose.SchemaTypes.Email,  
+            required: [true, 'L\'adresse email est obligatoire'],  // Ajout de required
+            unique: true,
+            lowercase: true,
         },
 
+        role: {
+            type: Array,
+        },
+        
         password: {
             type: String,
-            require: [true, 'mot de passe obligatoire'],
-            trim: true
+            required: [true, 'mot de passe obligatoire'],
+            trim: true,
+            minlength: [6, 'Le mot de passe doit contenir au moins 6 caractères']  // Ajout sécurité
 
+        },
+        dateCreation: {
+            type: Date,
+            default: Date.now,
+            immutable: true // Empêche toute modification après création
         }
     }
 )
+
+//ajout d'index sur quelque élements
+userSchema.index({ mail:1, nom:1, age:-1, });
+
+  
 
 module.exports = mongoose.model('User', userSchema)
 
--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -1,6 +1,9 @@
 const express = require("express");
-const user = require('../models/userModel')
+const user = require('../models/userModel');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+
+//bcrypt config
 
 
 // router.get('/', (req, res) => {
@@ -9,16 +12,28 @@ const router = express.Router();
 
 // création de route user
 router.post('/', async function (req, res) {
+    
     console.dir(res.headersSent) // false
     const newUser = new user(req.body)
-    try { 
+
+    try {
+
+        const checkuser = await user.findOne({ email: req.body.email});
+        console.dir(checkuser);
+        
+        //logique de hashage du mot de passe 
+        const hashedPassword = await bcrypt.hash(req.body.password, 12)
+        
+        //hashage du mot de passe
+        newUser.password = hashedPassword;
+        console.dir(newUser);
+
         const addedUser = await newUser.save();
         res.json({addedUser})
 
     } catch (error) {
         res.json({message: error.message})
     }
-    
     
     console.dir(res.headersSent) // true
   })
